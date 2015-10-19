00e142884b5358074f461e5539d313100cb37c4d
Adam Sifounakis <sifounak@gmail.com>
2015-10-19 02:21:46 -0400

    Updated version of configuration menu.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is the config page used before Square version 3.3.
+Configuration page for Square<br>
+Download Square here: https://apps.getpebble.com/applications/552597c506e80c0a4b00007f<br>
+<br>
+master branch is for development<br>
+gh-pages branch is for public release
--- js/main.js
+++ js/main.js
@@ -16,35 +16,60 @@ function submitHandler() {
 
 function loadOptions() {
 	var $textColorPicker = $('#textColorPicker');
+	var $backgroundColorPicker = $('#backgroundColorPicker');
 	var $invertColors = $('#invertColors');
+	var $showWeather = $('#showWeather');
 	var $shakeWeather = $('#shakeWeather');
 	var $useCelsius = $('#useCelsius');
+	var $vibeConnect = $('#vibeConnect');
+	var $vibeDisconnect = $('#vibeDisconnect');
+	var $reflectBatt = $('#reflectBatt');
 
 	if (localStorage.invertColors) {
 		$textColorPicker[0].value = localStorage.textColor;
+		$backgroundColorPicker[0].value = localStorage.backgroundColor;
 		$invertColors[0].checked = localStorage.invertColors === 'true';
+		$showWeather[0].checked = localStorage.showWeather === 'true';
 		$shakeWeather[0].checked = localStorage.shakeWeather === 'true';
 		$useCelsius[0].checked = localStorage.useCelsius === 'true';
+		$vibeConnect[0].checked = localStorage.vibeConnect === 'true';
+		$vibeDisconnect[0].checked = localStorage.vibeDisconnect === 'true';
+		$reflectBatt[0].checked = localStorage.reflectBatt === 'true';
 	}
 }
 
 function getAndStoreConfigData() {
 	var $textColorPicker = $('#textColorPicker');
+	var $backgroundColorPicker = $('#backgroundColorPicker');
 	var $invertColors = $('#invertColors');
+	var $showWeather = $('#showWeather');
 	var $shakeWeather = $('#shakeWeather');
 	var $useCelsius = $('#useCelsius');
+	var $vibeConnect = $('#vibeConnect');
+	var $vibeDisconnect = $('#vibeDisconnect');
+	var $reflectBatt = $('#reflectBatt');
 
 	var options = {
 		textColor: $textColorPicker.val(),
+		backgroundColor: $backgroundColorPicker.val(),
 		invertColors: $invertColors[0].checked,
+		showWeather: $showWeather[0].checked,
 		shakeWeather: $shakeWeather[0].checked,
-		useCelsius: $useCelsius[0].checked
+		useCelsius: $useCelsius[0].checked,
+		vibeConnect: $vibeConnect[0].checked,
+		vibeDisconnect: $vibeDisconnect[0].checked,
+		reflectBatt: $reflectBatt[0].checked
 	};
 
 	localStorage.textColor = options.textColor;
+	localStorage.backgroundColor = options.backgroundColor;
 	localStorage.invertColors = options.invertColors;
+	localStorage.showWeather = options.showWeather;
 	localStorage.shakeWeather = options.shakeWeather;
 	localStorage.useCelsius = options.useCelsius;
+	localStorage.vibeConnect = options.vibeConnect;
+	localStorage.vibeDisconnect = options.vibeDisconnect;
+	localStorage.reflectBatt = options.reflectBatt;
 
 	console.log('Got options: ' + JSON.stringify(options));
 	return options;
